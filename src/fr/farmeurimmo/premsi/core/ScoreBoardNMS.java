b34e6dd1d04e602a27bdd2721ba414b50c4d423d
Robin M <farmeurimmo@gmail.com>
2021-10-17 12:39:04 +0200

    Fix du ping qui saute dans le scoreboard

--- src/fr/farmeurimmo/premsi/core/ScoreBoardNMS.java
+++ src/fr/farmeurimmo/premsi/core/ScoreBoardNMS.java
@@ -14,6 +14,7 @@ import net.luckperms.api.model.user.User;
 public class ScoreBoardNMS {
 	
 	public static Map<Player, ScoreboardSign> boards = new HashMap<>();
+	public static Map<Player, Integer> pings = new HashMap<>();
 	
 	public static void MakeScoreBoardForPlayer(Player player) {
 	ScoreboardSign sb = new ScoreboardSign(player, "§6§lPremsiServ");
@@ -29,36 +30,12 @@ public class ScoreBoardNMS {
 		Suffix = user.getCachedData().getMetaData().getSuffix().replace("&l", "").replace("&", "§");
 	}
 	
-	int ping = 0;
-	String pingcolor = "§2";
-	try {
-		  Object entityPlayer = player.getClass().getMethod("getHandle").invoke(player);
-		  ping = (int) entityPlayer.getClass().getField("ping").get(entityPlayer);
-		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException | NoSuchFieldException e) {
-		  ((Throwable) e).printStackTrace();
-		}
-	if(ping >= 0 && ping <= 35) {
-		pingcolor = "§2";
-	}
-	if(ping >= 36 && ping <= 65) {
-		pingcolor = "§a";
-	}
-	if(ping >= 66 && ping <= 95) {
-		pingcolor = "§e";
-	}
-	if(ping >= 96 && ping <= 125) {
-		pingcolor = "§c";
-	}
-	if(ping >= 126) {
-		pingcolor = "§4";
-	}
-	
-	
 	int online = Bukkit.getServer().getOnlinePlayers().size();
+	String serveurname = Bukkit.getServerName();
 	sb.create();
 	sb.setLine(14, "  §eplay.premsiserv.com");
 	sb.setLine(13, "§1");
-	sb.setLine(12, "§fServeur §8» §c"+Bukkit.getServerName());
+	sb.setLine(12, "§fServeur §8» §c"+serveurname);
 	sb.setLine(11, "§fEn ligne §8»§f §c" + online);
 	sb.setLine(10, "§6§lServeur");
 	sb.setLine(9, "§6");
@@ -70,25 +47,69 @@ public class ScoreBoardNMS {
 		sb.setLine(6, "§fAbonnement §8» §cx");
 	}
 	sb.setLine(5, "§fGrade §8» §f"+Grade+" "+Suffix);
-	sb.setLine(4, "§fPing §8» §f" + pingcolor + ping);
+	sb.setLine(4, "§fPing §8» §f" + GetPlayerPingFormatted(player));
 	sb.setLine(3, "§6§l"+player.getName());
 	sb.setLine(2, "§f");
 
-	//...//
+	boards.put(player, sb);
 
-	boards.put(player, sb); //-> Important to update after//
-
-	/**
-	* Until line 15 !
-	* Limitation of the game.
-	*/
 	}
+	@SuppressWarnings("deprecation")
 	public static void UpdateScoreBoard() {
 		for (Entry<Player, ScoreboardSign> board : boards.entrySet()) {
+			
+			String Grade = "§7N/A";
+			String Suffix = "x";
+			
+			User user = main.api.getUserManager().getUser(board.getKey().getUniqueId());
+			if (user.getCachedData().getMetaData().getPrefix() != null) {
+				Grade = user.getCachedData().getMetaData().getPrefix().replace("&l", "").replace("&", "§").replace("&d✯", "");
+			}
+			if (user.getCachedData().getMetaData().getSuffix() != null) {
+				Suffix = user.getCachedData().getMetaData().getSuffix().replace("&l", "").replace("&", "§");
+			}
+			int online = Bukkit.getServer().getOnlinePlayers().size();
+			board.getValue().setLine(4, "§fPing §8» §f" + GetPlayerPingFormatted(board.getKey()));
+			board.getValue().setLine(11, "§fEn ligne §8»§f §c" + online);
+			board.getValue().setLine(5, "§fGrade §8» §f"+Grade);
+			
+			if(Suffix.equalsIgnoreCase("§d✯")) {
+				board.getValue().setLine(6, "§fAbonnement §8» §a✔");
+				} else {
+					board.getValue().setLine(6, "§fAbonnement §8» §cx");
+				}
+		    
+		}
+		Bukkit.getServer().getScheduler().scheduleAsyncDelayedTask(Bukkit.getPluginManager().getPlugin("PremsiLobby"), new Runnable() {
+			public void run() {
+				UpdateScoreBoard();
+			}
+		}, 40);
+	}
+	@SuppressWarnings("deprecation")
+	public static void UpdateScorePings() {
+		for(Player player : Bukkit.getOnlinePlayers()) {
 			int ping = 0;
-			String pingcolor = "§2";
 			try {
-				  Object entityPlayer = board.getKey().getClass().getMethod("getHandle").invoke(board.getKey());
+				  Object entityPlayer = player.getClass().getMethod("getHandle").invoke(player);
+				  ping = (int) entityPlayer.getClass().getField("ping").get(entityPlayer);
+				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException | NoSuchFieldException e) {
+				  ((Throwable) e).printStackTrace();
+				}
+			pings.put(player, ping);
+		}
+		Bukkit.getServer().getScheduler().scheduleAsyncDelayedTask(Bukkit.getPluginManager().getPlugin("PremsiLobby"), new Runnable() {
+			public void run() {
+				UpdateScorePings();
+			}
+		}, 40);
+	}
+	public static String GetPlayerPingFormatted(Player player) {
+		String pingcolor = "§2";
+		int ping = 0;
+		if(!pings.containsKey(player)) {
+			try {
+				  Object entityPlayer = player.getClass().getMethod("getHandle").invoke(player);
 				  ping = (int) entityPlayer.getClass().getField("ping").get(entityPlayer);
 				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException | NoSuchFieldException e) {
 				  ((Throwable) e).printStackTrace();
@@ -108,34 +129,29 @@ public class ScoreBoardNMS {
 			if(ping >= 126) {
 				pingcolor = "§4";
 			}
-			
-			String Grade = "§7N/A";
-			String Suffix = "";
-			
-			User user = main.api.getUserManager().getUser(board.getKey().getUniqueId());
-			if (user.getCachedData().getMetaData().getPrefix() != null) {
-				Grade = user.getCachedData().getMetaData().getPrefix().replace("&l", "").replace("&", "§").replace("&d✯", "");
-			}
-			if (user.getCachedData().getMetaData().getSuffix() != null) {
-				Suffix = user.getCachedData().getMetaData().getSuffix().replace("&l", "").replace("&", "§");
-			}
-			int online = Bukkit.getServer().getOnlinePlayers().size();
-			board.getValue().setLine(4, "§fPing §8» §f" + pingcolor + ping);
-			board.getValue().setLine(11, "§fEn ligne §8»§f §c" + online);
-			board.getValue().setLine(5, "§fGrade §8» §f"+Grade);
-			
-			if(Suffix.equalsIgnoreCase("§d✯")) {
-				board.getValue().setLine(6, "§fAbonnement §8» §a✔");
-				} else {
-					board.getValue().setLine(6, "§fAbonnement §8» §cx");
-				}
-		    
+			String formatteda = pingcolor+ping;
+			return formatteda;
 		}
-		Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Bukkit.getPluginManager().getPlugin("PremsiLobby"), new Runnable() {
-			public void run() {
-				UpdateScoreBoard();
-			}
-		}, 40);
+		ping = pings.get(player);
+		
+		if(ping >= 0 && ping <= 35) {
+			pingcolor = "§2";
+		}
+		if(ping >= 36 && ping <= 65) {
+			pingcolor = "§a";
+		}
+		if(ping >= 66 && ping <= 95) {
+			pingcolor = "§e";
+		}
+		if(ping >= 96 && ping <= 125) {
+			pingcolor = "§c";
+		}
+		if(ping >= 126) {
+			pingcolor = "§4";
+		}
+		
+		String formatted = pingcolor + ping;
+		return formatted;
 	}
 	public static void DeleteScoreBoard() {
 		for (Entry<Player, ScoreboardSign> board : boards.entrySet()) {
